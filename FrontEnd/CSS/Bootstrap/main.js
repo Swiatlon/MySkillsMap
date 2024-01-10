270f05bf503e2d11e992cd737a6d9fb0be59fdaa
unknown <wiercikdoc@gmail.com>
2024-01-10 11:39:54 +0100

    Added reveal animation

--- FrontEnd/CSS/Bootstrap/main.js
+++ FrontEnd/CSS/Bootstrap/main.js
@@ -1,46 +1,65 @@
 // Schedule Switching Mechanism
 
 const daysBoxesOrder = [
-    [0, 1, 2, 3, 4], // S
-    [4, 3, 2, 1, 0], // M
-    [1, 3, 4, 1, 0], // T
-    [4, 2, 1, 3, 0], // W
-    [4, 3, 2, 1, 0], // T
-    [3, 2, 1, 4, 0], // F
-    [1, 4, 0, 2, 3], // S
+  [0, 1, 2, 3, 4], // S
+  [4, 3, 2, 1, 0], // M
+  [1, 3, 4, 1, 0], // T
+  [4, 2, 1, 3, 0], // W
+  [4, 3, 2, 1, 0], // T
+  [3, 2, 1, 4, 0], // F
+  [1, 4, 0, 2, 3], // S
 ];
 
-const boxesContainer = document.querySelector("#boxesContainer");
-const boxes = [...document.querySelectorAll("#boxesContainer > .images-section-box")].filter((item, index) => index % 2);
-const btnGroup = [...document.querySelectorAll(".btn-group > input[name=btnradio]")];
+const boxesContainer = document.querySelector('#boxesContainer');
+const boxes = [...document.querySelectorAll('#boxesContainer > .images-section-box')].filter(
+  (item, index) => index % 2
+);
+const btnGroup = [...document.querySelectorAll('.btn-group > input[name=btnradio]')];
 
-btnGroup.forEach(btn => btn.addEventListener("change", function () {
-    const selectedDay = parseInt(this.id.replace("btnradio", "")) - 1;
+btnGroup.forEach((btn) =>
+  btn.addEventListener('change', function () {
+    const selectedDay = parseInt(this.id.replace('btnradio', '')) - 1;
     const order = daysBoxesOrder[selectedDay];
     reorderBoxes(order);
-}));
+  })
+);
 
 function reorderBoxes(order) {
-    boxesContainer.innerHTML = "";
-    order.forEach(index => {
-        boxesContainer.appendChild(boxes[index].cloneNode(true));
-        boxesContainer.appendChild(boxes[index].cloneNode(true));
-    });
+  boxesContainer.innerHTML = '';
+  order.forEach((index) => {
+    boxesContainer.appendChild(boxes[index].cloneNode(true));
+    boxesContainer.appendChild(boxes[index].cloneNode(true));
+  });
 }
 
 // Navigation Background change
 
 const navbar = document.querySelector('.navbar');
-const navlinks = [...document.querySelectorAll(".nav-link")];
-const logo = document.querySelector('.navbar-brand');
-console.log(navlinks);
 
 window.addEventListener('scroll', function () {
-    if (window.scrollY > 0) {
-        navbar.classList.remove("navbar-dark");
-        navbar.classList.add("navbar-light");
-    } else {
-        navbar.classList.remove("navbar-light");
-        navbar.classList.add("navbar-dark");
-    }
+  if (window.scrollY > 0) {
+    navbar.classList.remove('navbar-dark');
+    navbar.classList.add('navbar-light');
+  } else {
+    navbar.classList.remove('navbar-light');
+    navbar.classList.add('navbar-dark');
+  }
 });
+
+// Content reveal animation
+
+const revealElements = [...document.querySelectorAll('.reveal')];
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  revealElements.forEach((elem) => {
+    const windowHeight = window.innerHeight;
+    const revealTop = elem.getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      elem.classList.add('showAnimation');
+    }
+  });
+}
